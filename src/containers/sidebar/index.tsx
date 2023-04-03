df35754c97a5e2b9183361ceaa771ec5a61ce005
Luis Eduardo <eduvoices25@gmail.com>
2023-04-03 14:54:20 -0300

    estilizando a sidebar

--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BtnTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Eduvoices.png" alt="" />
-    <Titulo fontSize={20}>Luis Eduardo</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Luis Eduardo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Eduvoices
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BtnTema>Trocar tema</BtnTema>
+    </SidebarContainer>
   </aside>
 )
 
